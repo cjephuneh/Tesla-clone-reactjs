fa1a11f4f899dbf20da1f4c319e38957e4bb85c5
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-02 22:35:26 +0300

    coode

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Section from './Section'
+
+const Header = () => {
+  return (
+    <div>
+        <div>Header</div>
+        <section />
+    </div>
+  )
+}
+
+export default Header
