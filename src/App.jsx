02a817b005ea250adff80cf69524146df6536b7f
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-02 22:47:16 +0300

    coode

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import logo from "./assets/tesla.svg";
+import Home from './Home'
 import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <nav>
-          <h5>Model S</h5>
-          <h5>Model 3</h5>
-          <h5>Model X</h5>
-          <h5>Model Y</h5>
-          <h5>Solar Roof</h5>
-          <h5>Solar Panels</h5>
-        </nav>
-      </header>
+
+      <Home />
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import Section from './Section'
+
+import logo from "./assets/tesla.svg";
 
 const Header = () => {
   return (
-    <div>
-        <div>Header</div>
-        <section />
+    <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <nav>
+          <h5>Model S</h5>
+          <h5>Model 3</h5>
+          <h5>Model X</h5>
+          <h5>Model Y</h5>
+          <h5>Solar Roof</h5>
+          <h5>Solar Panels</h5>
+        </nav>
     </div>
   )
 }
