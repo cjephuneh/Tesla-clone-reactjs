8ae843923bf1072641bdf73c7c724e0d0f7ca066
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-07 11:10:19 +0300

    cooode

--- src/componets/Section.jsx
+++ src/componets/Section.jsx
@@ -1,14 +1,22 @@
 import './section.css'
-import logo from "./assets/tesla.svg"; 
+import styled from 'styled-components' 
 const Section = () =>{
     return(
-        <div className='wrap'>
-            <img src={logo} className="App-logo" alt="logo" />
-        </div>
+        <Wrap>
+            <h1>hello</h1>
+        </Wrap>
     )
 }
 
 
 export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: red;
+    background-image
+   
+`
 
    
