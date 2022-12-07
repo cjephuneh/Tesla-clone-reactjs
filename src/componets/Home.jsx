28dee017fdb8052c71a14862dfacb89ae17a57cd
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-08 01:57:12 +0300

    coode

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -5,7 +5,54 @@ import Section from './Section'
 const Home = () => {
   return (
     <Container>
-      <Section />
+      <Section 
+          title="Model S"
+          description="Schedule a Test Drive"
+          backgroundImg="model-s.jpg"
+          leftBtnText="Customer Order"
+          rightBtnText="Existing inventory"
+      />
+
+      <Section 
+          title="Model Y"
+          description="Schedule a Test Drive"
+          backgroundImg="model-y.jpg"
+          leftBtnText="Customer Order"
+          rightBtnText="Existing inventory"
+      />
+
+      <Section 
+          title="Model X"
+          description="Schedule a Test Drive"
+          backgroundImg="model-x.jpg"
+          leftBtnText="Customer Order"
+          rightBtnText="Existing inventory"
+      />
+
+      <Section 
+          title="Lowest Cost Solar Panels in Kenya"
+          description="Money-back guarantee"
+          backgroundImg="solar-panel.jpg"
+          leftBtnText="Order now"
+          rightBtnText="Learn more"
+      />
+
+      <Section 
+          title="Solar for New Roofs"
+          description="Solar Roof Costs Less Than a New Roof plus Solar Panels"
+          backgroundImg="solar-roof.jpg"
+          leftBtnText="Order now"
+          rightBtnText="Learn more"
+      /> 
+
+      <Section 
+          title="Accessories"
+          description=""
+          backgroundImg="accessories.jpg"
+          leftBtnText="Shop now"
+          
+      />
+      
     </Container>
 
     
