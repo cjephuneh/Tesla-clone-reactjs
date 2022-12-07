04e706281f9c95b3c61d030a7255fe4ad4ef77f7
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-08 02:26:42 +0300

    coode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Home from './componets/Home'
 import './App.css'
+import Header from "./componets/Header";
 
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
       <Home />
     </div>
   );
--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,21 +1,64 @@
 import React from 'react'
-
-import logo from "./assets/tesla.svg";
+import styled from 'styled-components'
 
 const Header = () => {
   return (
-    <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <nav>
-          <h5>Model S</h5>
-          <h5>Model 3</h5>
-          <h5>Model X</h5>
-          <h5>Model Y</h5>
-          <h5>Solar Roof</h5>
-          <h5>Solar Panels</h5>
-        </nav>
-    </div>
+    <Container>
+      <a>
+        <img src='/images/logo.svg' alt='' />
+      </a>
+      <Menu>
+          <a href="#">Model S </a>
+          <a href="#">Model 3 </a>
+          <a href="#">Model X </a>
+          <a href="#">Model Y </a>
+      </Menu>
+
+      <RightMenu>
+          <a href='#'>SHOP</a>
+          <a href='#'>TESLA ACCOUNT</a>
+
+      </RightMenu>
+    </Container>
   )
 }
 
 export default Header
+
+
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+
+  a{
+    font-weight: 600;
+    text_transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+    text-decoration: none;
+  }
+
+`
+
+const RightMenu = styled.div`
+a{
+  font-weight: 600;
+  text_transform: uppercase;
+  margin-right: 10px;
+  text-decoration: none;
+}
+`
