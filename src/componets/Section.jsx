cac8f8c295ac832a708f4fb29cef8da1a813e5b7
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-07 23:29:56 +0300

    cooode

--- src/componets/Section.jsx
+++ src/componets/Section.jsx
@@ -1,5 +1,6 @@
 import './section.css'
 import styled from 'styled-components' 
+import { ButtonGroup } from '@material-ui/core'
 const Section = () =>{
     return(
         <Wrap>
@@ -7,6 +8,16 @@ const Section = () =>{
                 <h1>Model S</h1>
                 <p>Schedule a Test Drive</p>
             </ItemText>
+            <ButtonGroup className='group'>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Order
+                </RightButton>
+            </ButtonGroup>
+            <DownArrow src="/images/down-arrow.svg"/>
+            
         </Wrap>
     )
 }
@@ -21,6 +32,10 @@ const Wrap = styled.div`
     background-positon: center;
     background-repeat: no-repeat;
     background-image: url('/images/model-s.jpg');  
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 `
 
 const ItemText = styled.div`
@@ -28,5 +43,28 @@ const ItemText = styled.div`
     text-align: center;
 
 `
+const DownArrow = styled.img`
+    margin-top: 20px;
+    height: 40px;
 
-   
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    
+`
+const RightButton = styled(LeftButton)`
+`
+
+
