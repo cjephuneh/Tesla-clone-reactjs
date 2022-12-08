5bacc59260939a6a3c6a417ff2300a1075c84554
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-08 03:59:53 +0300

    coode

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 const Header = () => {
+  const[burgerStatus, setBurgerStatus] = useState(false)
   return (
     <Container>
       <a>
@@ -18,11 +21,31 @@ const Header = () => {
       <RightMenu>
           <a href='#'>SHOP</a>
           <a href='#'>TESLA ACCOUNT</a>
-          <CustomMenu>
+          <CustomMenu onClick={() => setBurgerStatus(true)}>
             <MenuIcon />
           </CustomMenu>
 
       </RightMenu>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+            <CustomClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
+        
+        <li><a href='#'>Existing Inventory</a></li>
+        <li><a href='#'>Insurance</a></li>
+        <li><a href='#'>Used Inventory</a></li>
+        <li><a href='#'>Trade-in</a></li>
+        <li><a href='#'>Cyber</a></li>
+        <li><a href='#'>Roadaster</a></li>
+        <li><a href='#'>Semi</a></li>
+        <li><a href='#'>Charging</a></li>
+        <li><a href='#'>Power</a></li>
+        <li><a href='#'>Commercial Energy</a></li>
+        <li><a href='#'>Utilities</a></li>
+        <li><a href='#'>Find Us</a></li>
+        <li><a href='#'>Support</a></li>
+        <li><a href='#'>Investor Relations</a></li>
+      </BurgerNav>
     </Container>
   )
 }
@@ -41,6 +64,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index: 1;
 `
 
 const Menu = styled.div`
@@ -75,4 +99,43 @@ a{
 
 const CustomMenu = styled(MenuIcon)`
   cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top:0;
+  Bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  a{
+    font-weight: 600;
+    text_transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+    text-decoration: none;
+  }
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  li{
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, .2)
+  }
+
+  transform: ${props.show ? 'translateX(0)' : 'translateX(100%)' }
+  transfrom: transform 0.2s;
+
+
+`
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `
