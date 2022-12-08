fc42515f2d268d7cbda38c7381a246205865c463
caleb jephuneh <calebjephunneh3@gmail.com>
2022-12-08 04:01:14 +0300

    cooode

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -5,7 +5,7 @@ import CloseIcon from '@mui/icons-material/Close';
 
 
 const Header = () => {
-  const[burgerStatus, setBurgerStatus] = useState(false)
+  const[ burgerStatus, setBurgerStatus ] = useState(false)
   return (
     <Container>
       <a>
